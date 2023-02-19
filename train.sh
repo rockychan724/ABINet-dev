29438b0872ced030ae6ab56ec23ff3abdb7480cc
chenlei <ruyueshi@qq.com>
2023-02-19 13:42:20 +0800

    Unify the format of configuration files.

--- train.sh
+++ train.sh
@@ -2,10 +2,16 @@
 # v0.8
 # CUDA_VISIBLE_DEVICES=0,1 python main.py --config=configs/train_abinet.yaml
 # v0.9
-CUDA_VISIBLE_DEVICES=0,1 python main.py --config=configs/train_abinet_wo_iter.yaml
+# CUDA_VISIBLE_DEVICES=0,1 python main.py --config=configs/train_abinet_wo_iter.yaml
+# v1.1
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config=configs/train_abinet_v1.1.yaml
 
 # Pre-train Vision model
-# CUDA_VISIBLE_DEVICES=0,1 python main.py --config=configs/pretrain_vision_model.yaml
+# v0.5
+# CUDA_VISIBLE_DEVICES=0,1 python main.py --config=configs/pretrain_vision_model_v0.5.yaml
+# v1.0
+# CUDA_VISIBLE_DEVICES=0,1 python main.py --config=configs/pretrain_vision_model_v1.0.yaml
 
 # Pre-train Language model
+# v0.7
 # CUDA_VISIBLE_DEVICES=0,1 python main.py --config=configs/pretrain_language_model.yaml
